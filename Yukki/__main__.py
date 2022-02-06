93be9d8959100273ade796567ee0d808e8b4312c
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-07 00:23:42 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -184,7 +184,6 @@ async def start_command(_, message):
             )
     out = private_panel()
     return await message.reply_text(
-        home_text_pm,
         reply_markup=InlineKeyboardMarkup(out[1]),
     )
 
