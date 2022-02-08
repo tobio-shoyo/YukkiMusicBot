1ee9d74d8b0a0585255198e969b570ca16f612cf
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2022-02-08 15:02:31 +0530

    Update Auth.py

--- Yukki/Plugins/Auth.py
+++ Yukki/Plugins/Auth.py
@@ -9,23 +9,6 @@ from Yukki.Decorators.admins import AdminActual
 from Yukki.Utilities.changers import (alpha_to_int, int_to_alpha,
                                       time_to_seconds)
 
-__MODULE__ = "Auth Users"
-__HELP__ = """
-
-**Note:**
--Auth users can skip, pause, stop, resume Voice Chats even without Admin Rights.
-
-
-/auth [Username or Reply to a Message] 
-- Add a user to AUTH LIST of the group.
-
-/unauth [Username or Reply to a Message] 
-- Remove a user from AUTH LIST of the group.
-
-/authusers 
-- Check AUTH LIST of the group.
-"""
-
 
 @app.on_message(filters.command("auth") & filters.group)
 @AdminActual
